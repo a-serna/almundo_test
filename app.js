a251be690ff598e30731f7546c386aa170077dd0
a-serna <alejo.tanatos@gmail.com>
2017-03-14 02:13:26 -0500

    organizando filtrados

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ aplication.controller('hotelController',function($scope){
       name:'Hotel Emperador',
       stars:'3',
       price:'1596',
-      img:'assets/img/HotelEmperador.jpg'},
+      img:'assets/img/HotelEmperador.jpg',
+      aviso01: 'Precio por noche por habitación',
+      aviso02: 'Impuestos y tasas no incluidos',
+      pago01: 'Pagá en cuotas',
+      pago02: 'Pagá en destino'},
     {
       name:'Petit Palace San Bernardo',
       stars:'4',
       price:'2145',
-      img:'assets/img/HotelEmperador.jpg'},
+      img:'assets/img/HotelSanBernardo.jpg',
+      aviso01: 'Precio por noche por habitación',
+      aviso02: 'Impuestos y tasas no incluidos',
+      pago02: 'Pagá en destino'},
     {
       name:'Hotel Nuevo Boston',
       stars:'2',
       price:'861',
-      img:'assets/img/HotelEmperador.jpg'
+      img:'assets/img/HotelNuevoBoston.jpg',
+      aviso01: 'Precio por noche por habitación',
+      aviso02: 'Impuestos y tasas no incluidos',
+      detalle01: 'Precio sin promo',
+      sinPromo: 'ARS 1,118',
+      pago01: 'Pagá en cuotas'
     }];
 });
