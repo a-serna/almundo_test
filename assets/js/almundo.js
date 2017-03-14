5176a0864d309fe346b06ed07bf52793ab707061
a-serna <alejo.tanatos@gmail.com>
2017-03-14 03:57:02 -0500

    app hoteles finish

--- assets/js/almundo.js
+++ assets/js/almundo.js
@@ -4,17 +4,4 @@ $(document).ready(function(){
   edge: 'right',
   });
   $('.collapsible').collapsible();
-  // var slider = document.getElementById('range-input');
-  // noUiSlider.create(slider, {
-  //  start: [780, 1763],
-  //  connect: true,
-  //  step: 1,
-  //  range: {
-  //    'min': 750,
-  //    'max': 2000
-  //  },
-  //  format: wNumb({
-  //    decimals: 0
-  //  })
-  // });
 });
